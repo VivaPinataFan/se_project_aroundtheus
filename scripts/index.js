805aab96efbc9c60b5e32f5bef785e004c5ad9b5
VivaPinataFan <114098252+VivaPinataFan@users.noreply.github.com>
2023-04-08 10:21:30 -0600

    errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,30 @@
 const initialCards = [
-  { name: "Yosemite Valley", link: "../images/yosemite.jpg" },
-  { name: "Lake Louise", link: "../images/lake-louise.jpg" },
-  { name: "Bald Mountains", link: "../images/bald-mountains.jpg" },
-  { name: "Latemar", link: "../images/latemar.jpg" },
-  { name: "Vanoise National Park", link: "../images/vanoise.jpg" },
-  { name: "Lago di Braies", link: "../images/lago.jpg" },
+  { 
+    name: "Yosemite Valley", 
+    link: "../images/yosemite.jpg" 
+  },
+  { 
+    name: "Lake Louise", 
+    link: "../images/lake-louise.jpg" 
+  },
+  { 
+    name: "Bald Mountains", 
+    link: "../images/bald-mountains.jpg" 
+  },
+  { 
+    name: "Latemar", 
+    link: "../images/latemar.jpg" 
+  },
+  { 
+    name: "Vanoise National Park", 
+    link: "../images/vanoise.jpg" 
+  },
+  { 
+    name: "Lago di Braies", 
+    link: "../images/lago.jpg", 
+  }
 ];
+
+const profileEditButton = document.querySelector('#profile-button-edit');
+
+console.log(profileEditButton);
