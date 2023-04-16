13a45bdf177a00122d2cdd613065429a38eeaf79
VivaPinataFan <114098252+VivaPinataFan@users.noreply.github.com>
2023-04-16 09:07:30 -0600

    second submit

--- scripts/index.js
+++ scripts/index.js
@@ -42,7 +42,7 @@ const cardTemplate =
 // functions
 
 function closePopup() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -71,7 +71,7 @@ function handleProfileEditSubmit(e) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileSubtitleInput.value = profileSubtitle.textContent;
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal_opened");
 });
 
 modalCloseButton.addEventListener("click", () => {
